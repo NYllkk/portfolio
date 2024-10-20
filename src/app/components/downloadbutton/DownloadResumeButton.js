f6aee59cb3493838b2110f410cdd34efc4051113
Nikhil <nikhilsharma52197@gmail.com>
2024-10-21 01:08:51 +0530

    chnages

--- src/app/components/downloadbutton/DownloadResumeButton.js
+++ src/app/components/downloadbutton/DownloadResumeButton.js
@@ -2,7 +2,7 @@
 import React from 'react';
 "C:\Users\NIKHIL SHARMA\Downloads\Er.Nikhil.Dev.Resume.pdf (1).pdf"
 import { Icon } from '@iconify/react/dist/iconify.js';
-import {} from "/resume.pdf"
+// import {} from "/resume.pdf"
 const DownloadResumeButton = () => {
     return (
         <div >
